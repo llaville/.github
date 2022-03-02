1bc33fdbbd918faad7443aede9f7f98a43a28414
Laurent Laville <laurent.laville@gmail.com>
2022-03-02 10:02:06 +0000

    add resources to draw UML graphes for documentation (see hook script and gh-pages workflow)

---
+++ resources/graph-uml/datasource/component.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the TEMPLATE package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @since Release 1.0.0
+ * @author Laurent Laville
+ */
+
+function dataSource(): Generator
+{
+    $classes = [
+    ];
+    foreach ($classes as $class) {
+        yield $class;
+    }
+}
