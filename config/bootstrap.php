6116de5f25db239c5449baf47f985857090c696a
Laurent Laville <laurent.laville@gmail.com>
2022-10-31 07:40:50 +0100

    add templates for PHPUnit

---
+++ config/bootstrap.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the TEMPLATE package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Laurent Laville
+ */
+
+$autoloader = 'vendor/autoload.php';
+
+if (Phar::running()) {
+    $phar = new Phar($_SERVER['argv'][0]);
+    $possibleAutoloadPaths = [
+        'phar://' . $phar->getAlias(),
+    ];
+} else {
+    $possibleAutoloadPaths = [
+        // local dev repository
+        dirname(__DIR__),
+        // dependency
+        dirname(__DIR__, 4),
+    ];
+}
+
+$isAutoloadFound = false;
+foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
+    if (file_exists($possibleAutoloadPath . DIRECTORY_SEPARATOR . $autoloader)) {
+        require_once $possibleAutoloadPath . DIRECTORY_SEPARATOR . $autoloader;
+        $isAutoloadFound = true;
+        $vendorDir = $possibleAutoloadPath . DIRECTORY_SEPARATOR . dirname($autoloader);
+        break;
+    }
+}
+
+if ($isAutoloadFound === false) {
+    throw new RuntimeException(
+        sprintf(
+            'Unable to find "%s" in "%s" paths.',
+            $autoloader,
+            implode('", "', $possibleAutoloadPaths)
+        )
+    );
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the TEMPLATE package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * TestClass to place utility functions in.
+ *
+ * @author Laurent Laville
+ * @since Release 1.0.0
+ */
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
