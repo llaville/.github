8dbc095e016fefd3151397a0bff4120788f5ff78
Laurent Laville <laurent.laville@gmail.com>
2024-01-01 14:42:12 +0000

    upgrade resources build system (UML graphes)

--- resources/gh-pages-hook.sh
+++ resources/gh-pages-hook.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ASSETS_IMAGE_DIR="docs/assets/images"
 
-php $SCRIPT_DIR/graph-uml/build.php component $ASSETS_IMAGE_DIR
+php $SCRIPT_DIR/build.php graph-composer $ASSETS_IMAGE_DIR
